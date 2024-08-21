025f5ad6e9f1c316e43ed5662e7fb98cdd2b8c82
adimerpaul <adimer101@gmail.com>
2024-08-21 06:06:46 -0400

    Actulizado el proyecto hast la creacion de pestañas

--- back/app/Http/Controllers/PedidoController.php
+++ back/app/Http/Controllers/PedidoController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pedido;
 use Illuminate\Http\Request;
 
-class PedidoController extends Controller
-{
-    //
+class PedidoController extends Controller{
+    function index(){
+        return Pedido::all();
+    }
+    function store(Request $request){
+        return Pedido::create($request->all());
+    }
+    function show($id){
+        return Pedido::find($id);
+    }
+    function update(Request $request, $id){
+        $pedido = Pedido::find($id);
+        $pedido->update($request->all());
+        return $pedido;
+    }
+    function destroy($id){
+        return Pedido::destroy($id);
+    }
 }
--- back/routes/api.php
+++ back/routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\PedidoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('pedidos', [PedidoController::class, 'index']);
+Route::post('pedidos', [PedidoController::class, 'store']);
+Route::get('pedidos/{id}', [PedidoController::class, 'show']);
+Route::put('pedidos/{id}', [PedidoController::class, 'update']);
+Route::delete('pedidos/{id}', [PedidoController::class, 'destroy']);
