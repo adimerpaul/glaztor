eb24e38c83139717ddac53e4a783e3ce9a828f62
adimerpaul <adimer101@gmail.com>
2024-08-22 05:46:17 -0400

    actulziado botones

--- front/src/pages/preventas/Preventa.vue
+++ front/src/pages/preventas/Preventa.vue
@@ -1,19 +1,6 @@
 <template>
     <q-page >
         {{preventas}}
-
-        <div style="height: 75vh; width: 50vw;">
-            <l-map
-            v-model="zoom"
-            v-model:zoom="zoom"
-            :center="[47.41322, -1.219482]"
-            @move="log('move')"
-            >
-            <l-tile-layer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            ></l-tile-layer>
-            </l-map>
-        </div>
     </q-page>
     <q-page-sticky position="bottom-right" class="text-bold" :offset="[18, 18]">
         <q-btn fab icon="add" color="primary" @click="dialogClick" />
@@ -91,18 +78,11 @@
 <script>
 import moment from 'moment'
 import { Loading } from 'quasar';
-import "leaflet/dist/leaflet.css";
-import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
 
 export default {
     name: 'Preventa',
-    components: {
-        LMap,
-        LTileLayer,
-    },
     data () {
         return {
-            zoom: 2,
             propietarioBtnBool: true,
             encargadoBtnBool: true,
             preventas: [],
