28245174263996830b2002d035cd20afc3a4ae21
adimer <adimer101@gmail.com>
2024-11-24 03:54:21 -0400

    Actulizado para el login

--- back/database/seeders/DatabaseSeeder.php
+++ back/database/seeders/DatabaseSeeder.php
@@ -14,10 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
+        User::create(['name' => 'Adminsitrador','username' => 'admin','password' => bcrypt('admin'),'role' => 'Adminsitrador']);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
--- front/src/pages/IndexPage.vue
+++ front/src/pages/IndexPage.vue
@@ -2,15 +2,15 @@
   <q-page class="flex flex-center">
     <img
       alt="Glaztor logo"
-      src="\public\logo.png"
+      src="logo.png"
       style="width: 200px; height: 200px"
     >
     <div class="text-h2 text-blue" style="opacity:.9">
       DISTRIBUIDOR OFICIAL
      </div>
      </q-page>
-    
-  
+
+
 </template>
 
 <script setup>
